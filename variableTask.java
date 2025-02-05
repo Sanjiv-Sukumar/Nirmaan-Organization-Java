8bc48c5914e9d810e897b87d3a9762a931635f70
Sanjiv-Sukumar <sanjivshivu@gmail.com>
2025-02-05 16:08:59 +0530

    day1

---
+++ variableTask.java
@@ -0,0 +1,25 @@
+package Day1;
+
+public class variableTask {
+	public static void main(String args[]){
+		byte b = 007;
+		System.out.println("byte : "+ b);
+		short s = 3227;
+		System.out.println("short : "+s);
+		int num = 70929383;
+		System.out.println("int : "+ num);
+		long lg = 89455564892454l;
+		System.out.println("long : "+lg);
+		float fl = 528.43f;
+		System.out.println("float : "+fl);
+		double db = 25.487564d;
+		System.out.println("double : "+db); 
+		boolean bool = true;
+		System.out.println("boolean : "+bool); 
+		char ch = 'S';
+		System.out.println("char : "+ ch);
+		String str = "Sanjiv S";
+		System.out.println("name : "+ str);
+	}
+
+}
